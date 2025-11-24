72de1e1859c391b6b9ebd5fbaafd4cefde5f335f
Leonardo Rigotti <leorigo@MacBook-Pro-di-Leonardo-2.local>
2025-11-24 12:26:46 +0100

    ,

--- svd_qr.c
+++ svd_qr.c
@@ -254,14 +254,12 @@ void QR_SVD(double A[][N], MPI_Comm comm){
     }
 
     if(rank == 0){
-
-        
         int mat_rank = min(N, M);
         printf("Eigenvalues:");
         for (size_t i = 0; i < N; i++){
             printf("\n");
             for (size_t j = 0; j < N; j++){
-                if(i == j) printf("%f   ", eigvals[i][j]);
+                printf("%f   ", eigvals[i][j]);
             }
         }
 
