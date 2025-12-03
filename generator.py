dfd47ac125ab72f219f7ab5691f65470430397c7
Leonardo Rigotti <leorigo@MacBook-Pro-di-Leonardo-2.local>
2025-12-03 13:06:19 +0100

    New dataset

--- generator.py
+++ generator.py
@@ -30,7 +30,7 @@ def generate_and_save_dataset():
             for i in range(NUM_MATRICES):
                 # Randomly determine dimensions (e.g., between 2x2 and 10x10)
                 R = random.randint(2, 2)
-                C = random.randint(2, 2)
+                C = random.randint(2, 5)
                 
                 # Generate a random matrix with real (float) values
                 # Values will be between -10.0 and 10.0 for variety
