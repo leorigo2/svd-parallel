43a940e78b5e1ac90cd6609bb29dae48addf7a69
Leonardo Rigotti <leorigo@MacBook-Pro-di-Leonardo-2.local>
2025-12-10 14:43:46 +0100

    Add one time bcasting for matrix A

--- generator.py
+++ generator.py
@@ -29,8 +29,8 @@ def generate_and_save_dataset():
             
             for i in range(NUM_MATRICES):
                 # Randomly determine dimensions (e.g., between 2x2 and 10x10)
-                R = random.randint(5, 10)
-                C = random.randint(5, 10)
+                R = random.randint(500, 1000)
+                C = random.randint(500, 1000)
                 
                 # Generate a random matrix with real (float) values
                 # Values will be between -10.0 and 10.0 for variety
