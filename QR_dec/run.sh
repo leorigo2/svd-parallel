365d36743ce402098460ad17d1725bee629fcc50
Rigotti <leonardo.rigotti@hpc-head-n2.unitn.it>
2025-12-17 09:44:49 +0100

    Results with R greater than C

--- QR_dec/run.sh
+++ QR_dec/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#PBS -l select=4:ncpus=5:mem=2gb 
+#PBS -l select=2:ncpus=16:mem=2gb 
 # set max execution time
 #PBS -l walltime=5:00:00
 # set the queue
 #PBS -q short_HPC4DS
 module load mpich-3.2
-mpiexec -n 20 ./svd-parallel/QR_dec/svd_qr_parallel 
+mpiexec -n 32 ./svd-parallel/QR_dec/svd_qr_parallel 
--- QR_dec/svd_qr_parallel.c
+++ QR_dec/svd_qr_parallel.c
@@ -433,7 +433,7 @@ int main(int argc, char* argv[]){
 
     if(my_rank == 0){
         dataset = fopen("./svd-parallel/dataset.txt", "r");
-        results = fopen("./svd-parallel/QR_results/results_parallel_4x5.txt", "w");
+        results = fopen("./svd-parallel/QR_results/r_greater_c/results_parallel_2x16.txt", "w");
 
         fprintf(results, "elements time\n");
 
